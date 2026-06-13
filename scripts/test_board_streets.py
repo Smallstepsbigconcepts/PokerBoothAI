02070615aeb0e16793daefcedf837979bdad1d16
Smallstepsbigconcepts <134173702+Smallstepsbigconcepts@users.noreply.github.com>
2026-06-13 09:00:12 -0400

    Update board state test

--- scripts/test_board_streets.py
+++ scripts/test_board_streets.py
@@ -4,20 +4,12 @@ import os
 
 sys.path.append(os.path.dirname(__file__))
 
-from board_detector import extract_board_cards
+from board_detector import extract_board_state
 
 image_path = "/content/drive/MyDrive/PokerBoothAI/Reports/phase1_review_frames/frame_0176_01-28-00.jpg"
 
 frame = cv2.imread(image_path)
 
-cards = extract_board_cards(frame)
+board = extract_board_state(frame)
 
-ranks = [card["rank"] for card in cards]
-
-flop = ranks[:3]
-turn = ranks[3]
-river = ranks[4]
-
-print("Flop:", flop)
-print("Turn:", turn)
-print("River:", river)
+print(board)
